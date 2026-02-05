c886870fdbc205f227b70f3227d297eca325438e
Paterne <diedhioupaterne279@gmail.com>
2026-02-05 12:28:30 +0000

    fix :allow host

--- devops/settings.py
+++ devops/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-59&zamrt&9wm)(47)wuu0&c$(tmwf#vn&(dodoo!1&wk#19fpu
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
